a207e85a6bac69fa73d7b02295f3a00c58e98008
Al Thompson <athompson@ngmoco.com>
2012-11-09 12:31:10 -0800

    This revision includes changes to implement a full integration test in mobilize_test.rb.
    Also included is some test suite runner prototyping in the mobilize_idea.rb file.

--- test/mobilize_test.rb
+++ test/mobilize_test.rb
@@ -1,15 +1,39 @@
 require 'test_helper'
+
 describe "Mobilize" do
-  before do
-    puts "before"
+  
+  def before
+    puts 'before'
+
   end
 
-  it "preps 4 workers on Jobtracker" do
+  # enqueues 4 workers on Resque
+  it "runs integration test" do
+    email = Mobilize::Gdriver.owner_email
+
+    puts 'enqueues 4 workers on Resque'
     Mobilize::Jobtracker.prep_workers
     sleep 10
     assert Mobilize::Jobtracker.workers.length == Mobilize::Resque.config['max_workers'].to_i
-  end
+    puts "creates requestor 'mobilize'"
 
+    requestor = Mobilize::Requestor.find_or_create_by_email(email)
+    assert requestor.email == email
+
+    puts "TODO: enqueues jobtracker" 
+
+    puts "TODO: requestor creates specbook"
+
+    puts "TODO: jobtracker creates jobspec with 'jobs' sheet with headers"
+    
+    puts "TODO: runs test job with test source gsheet"
+      
+    puts "TODO: verify that jobtracker posts tests source to test destination"
+
+    # clean up
+    Mobilize::Requestor.find_or_create_by_email(email).delete      
+  end
+    
   after do
     processes = `ps -A -o pid,command | grep [r]edis-test`.split($/)
     pids = processes.map { |process| process.split(" ")[0] }
@@ -17,5 +41,4 @@ describe "Mobilize" do
     pids.each { |pid| Process.kill("TERM", pid.to_i) }
     `rm -f #{$dir}/dump.rdb #{$dir}/dump-cluster.rdb`
   end
-
 end
