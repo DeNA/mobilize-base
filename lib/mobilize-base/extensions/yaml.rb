ee0b296fd16ef8393bdb8511245574bb7df945d6
Cassio Paes-Leme <cpaesleme@ngmoco.com>
2013-01-28 10:52:02 -0800

    fixed easy load url munging issue

--- lib/mobilize-base/extensions/yaml.rb
+++ lib/mobilize-base/extensions/yaml.rb
@@ -1,11 +1,17 @@
 module YAML
   def YAML.easy_load(string)
     begin
-      YAML.load(s.param_string)
+      return YAML.load(s.param_string)
     rescue
       #replace colon w space colon, double space colons w single space
       gsub_colon_string = string.gsub(":",": ").gsub(":  ",": ")
-      YAML.load("{#{gsub_colon_string}}")
+      easy_hash = YAML.load("{#{gsub_colon_string}}")
+      #make sure urls have their colon spaces fixed
+      result_hash={}
+      easy_hash.each do |k,v|
+        result_hash[k] = v.gsub(": //","://")
+      end
+      return result_hash
     end
   end
 end
--- lib/mobilize-base/version.rb
+++ lib/mobilize-base/version.rb
@@ -1,5 +1,5 @@
 module Mobilize
   module Base
-    VERSION = "1.1.02"
+    VERSION = "1.1.03"
   end
 end
