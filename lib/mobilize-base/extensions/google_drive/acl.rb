741bfec0afed01201604f1c3b9d4c866f9bdbb5e
Cassio Paes-Leme <cpaesleme@ngmoco.com>
2013-05-09 15:31:02 -0700

    once again

--- lib/mobilize-base/extensions/google_drive/acl.rb
+++ lib/mobilize-base/extensions/google_drive/acl.rb
@@ -14,9 +14,9 @@ module GoogleDrive
     def push(entry)
       #do not send email notifications
       entry = AclEntry.new(entry) if entry.is_a?(Hash)
-      url_suffix = "?send-notification-emails=false"
+      url_suffix = "&send-notification-emails=false"
       header = {"GData-Version" => "3.0", "Content-Type" => "application/atom+xml"}
-      doc = @session.request(:post, "#{:}#{url_suffix}", :data => entry.to_xml(), :header => header, :auth => :writely)
+      doc = @session.request(:post, "#{@acls_feed_url}#{url_suffix}", :data => entry.to_xml(), :header => header, :auth => :writely)
       entry.params = entry_to_params(doc.root)
       @acls.push(entry)
       return entry
