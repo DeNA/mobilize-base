2f4ba606d665814a3734c1cc0b793270532ad514
Cassio Paes-Leme <cpaesleme@ngmoco.com>
2013-05-15 16:59:45 -0700

    moved mobilize_env to rake task parameters

--- lib/mobilize-base/tasks.rb
+++ lib/mobilize-base/tasks.rb
@@ -1,6 +1,7 @@
 namespace :mobilize_base do
   desc "Start a Resque worker"
-  task :work do
+  task :work, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Base.config('jobtracker')['extensions'].each do |e|
       begin
@@ -20,53 +21,69 @@ namespace :mobilize_base do
     worker.work(ENV['INTERVAL'] || 5) # interval, will block
   end
   desc "Kill all Resque workers"
-  task :kill_workers do
+  task :kill_workers, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Jobtracker.kill_workers
   end
   desc "Kill idle workers not in sync with repo"
-  task :kill_idle_and_stale_workers do
+  task :kill_idle_and_stale_workers, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Jobtracker.kill_idle_and_stale_workers
   end
-    desc "Kill idle workers"
-    task :kill_idle_workers do
+  desc "Kill idle workers"
+  task :kill_idle_workers, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Jobtracker.kill_idle_workers
   end
   desc "Make sure there are the correct # of workers, kill if too many"
-  task :prep_workers do
+  task :prep_workers, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Jobtracker.prep_workers
   end
   desc "Stop Jobtracker"
-  task :stop_jobtracker do
+  task :stop_jobtracker, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Jobtracker.stop!
   end
   desc "Start Jobtracker"
-  task :start_jobtracker do
+  task :start_jobtracker, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Jobtracker.start
   end
   desc "Restart Jobtracker"
-  task :restart_jobtracker do
+  task :restart_jobtracker, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::Jobtracker.restart!
   end
+  desc "Add a user"
+  task :enqueue_user, :name, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
+    require 'mobilize-base'
+    Mobilize::User.where(name: args.name).first.runner.enqueue!
+  end
   desc "Enqueue a user's runner"
-  task :enqueue_user, [:name] do |t,args|
+  task :enqueue_user, :name, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     Mobilize::User.where(name: args.name).first.runner.enqueue!
   end
   desc "Enqueue a stage"
-  task :enqueue_stage, [:path] do |t,args|
+  task :enqueue_stage, :path, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     user,job,stage = args.path.split("/")
     Mobilize::Stage.where(path: "Runner_#{user}/jobs/#{job}/#{stage}").first.en
   end
   desc "kill all old resque web processes, start new one with resque_web.rb extension file"
-  task :resque_web do
+  task :resque_web, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     port = Mobilize::Base.config('resque')['web_port']
     config_dir = (ENV['MOBILIZE_CONFIG_DIR'] ||= "config/mobilize/")
@@ -94,14 +111,16 @@ namespace :mobilize_base do
     `#{command}`
   end
   desc "create indexes for all base models in mongodb"
-  task :create_indexes do
+  task :create_indexes, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     require 'mobilize-base'
     ["Dataset","Job","Runner","Task","User"].each do |m|
       "Mobilize::#{m}".constantize.create_indexes
     end
   end
   desc "Set up config and log folders and files, populate from samples"
-  task :setup do
+  task :setup, :env do |t,args|
+    ENV['MOBILIZE_ENV']=args.env
     config_dir = (ENV['MOBILIZE_CONFIG_DIR'] ||= "config/mobilize/")
     log_dir = (ENV['MOBILIZE_LOG_DIR'] ||= "log/")
     sample_dir = File.dirname(__FILE__) + '/../samples/'
